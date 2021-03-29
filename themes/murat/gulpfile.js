74e0ab328f3824e2c220937d6b395516c133fcdb
Egor Babintsev <e.e.babintsev@yandex.ru>
2021-03-29 11:50:12 +0300

    Add gulp task for minifying js files

--- themes/murat/gulpfile.js
+++ themes/murat/gulpfile.js
@@ -1,11 +1,20 @@
 const gulp = require('gulp')
 const less = require('gulp-less')
 const browserSync = require('browser-sync')
+const uglify = require('gulp-uglify')
+const rename = require('gulp-rename')
 
 gulp.task('less', () => {
   return gulp.src('css/less/style.less')
     .pipe(less())
     .pipe(gulp.dest('./css/'))
+})
+
+gulp.task('js', () => {
+  return gulp.src(['js/*.js', '!js/*.min*.js'])
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('js/'))
 })
 
 gulp.task('browser-sync', () => {
@@ -20,6 +29,7 @@ gulp.task('browser-sync', () => {
 
 gulp.task('watch', () => {
   gulp.watch('./css/less/**/*.less', gulp.series('less'))
+  gulp.watch(['js/*.js', '!js/*.min*.js'], gulp.series('js'))
 })
 
 gulp.task('default', gulp.parallel('watch', 'browser-sync'))
